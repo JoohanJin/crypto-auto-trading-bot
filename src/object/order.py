add5b714809bfae51b17919c610af1d047dbe01c
JoohanJIN <wngks8730@gmail.com>
2025-12-27 18:32:49 +0800

    feat: Custom DTOs and Contants clean up and re categorization

--- src/object/order.py
+++ src/object/order.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from __future__ import annotations
+from dataclasses import dataclass, replace
 from enum import IntFlag
 
 
@@ -18,20 +19,23 @@ class OrderHistory:
 
 
 class OrderType(IntFlag):
-    BUY = 1  # 1
-    SELL = 2  # 10
+    BUY = 1  # 0001
+    SELL = 2  # 0010
 
 
 @dataclass
 class Order:
-    type: OrderType
-    type_str: str
+    type: OrderType  # BUY or SELL
+    type_str: str  # "BUY" or "SELL"
     leverage: int
-    entry_price: float
-    tp_price: float
-    sl_price: float
+    entry_price: float  # Entry price - current price
+    tp_price: float  # Take Profit Price: + 20% for reverse ordering enabled borker
+    sl_price: float  # Stop Loss Price: -20% for reverse ordering enblaed broker
     ticker: str
     ticker_size: float
     quote: str
     quote_size: float
     meta_data: dict | None  # to keep the metadata in the form of json.
+
+    def copy(self) -> Order:
+        return replace(self)
