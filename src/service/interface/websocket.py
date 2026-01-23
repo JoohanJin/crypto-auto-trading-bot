9a5ad18b1d50f9ab8153f0e11b463ca3173022af
JoohanJIN <wngks8730@gmail.com>
2026-01-23 21:32:00 +0800

    feat: interface for websocket api

---
+++ src/service/interface/websocket.py
@@ -0,0 +1,8 @@
+from service.mexc.future import FutureWebSocketClient as MexcFutureWebSocketClient
+from service.binance.future import FutureWebSocketClient as BinanceFutureWebSocketClient
+
+
+class WebSocketInterface:
+    def __init__(self) -> None:
+
+        return
