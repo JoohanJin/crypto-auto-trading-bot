49792d4ee3a8cb8698e1d55cebec7486848ed2ba
JoohanJIN <wngks8730@gmail.com>
2026-01-25 03:49:44 +0800

    feat: WebSocketClientRegistry has been added

--- src/service/interface/websocket.py
+++ src/service/interface/websocket.py
@@ -1,8 +1,40 @@
-from service.mexc.future import FutureWebSocketClient as MexcFutureWebSocketClient
-from service.binance.future import FutureWebSocketClient as BinanceFutureWebSocketClient
+from service.sdk.websocket_sdk import WebSocketClient
+
+
+class WebSocketClientRegistry:
+    def __init__(
+        self,
+        name: str,
+    ) -> None:
+        self._registry: dict[str, WebSocketClient] = dict()
+        return
+
+    def push(
+        self,
+        key: str,
+        client: WebSocketClient,
+    ) -> None:
+        self._registry[key] = client
+        return
+
+    def get(
+        self,
+        key: str,
+    ) -> WebSocketClient | None:
+        return self._registry.get(key, None)
+
+    def pop(
+        self,
+        key: str,
+    ) -> None:
+        self._registry.pop(key, None)
+        return
+
+    @property
+    def registry(self):
+        return self._registry
 
 
 class WebSocketInterface:
     def __init__(self) -> None:
-
         return
