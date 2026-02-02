7ca7c548a1298c9dd707cf1ed1a5021dfde36eac
JoohanJIN <wngks8730@gmail.com>
2026-02-02 21:57:15 +0800

    feat: added the __repr__ function for TradePair

--- src/core/models/trade.py
+++ src/core/models/trade.py
@@ -26,3 +26,6 @@ class TradePair:
 
     def copy(self) -> TradePair:
         return replace(self)
+
+    def __repr__(self) -> str:
+        return f"TradePair object - ticker: {self.ticker} - quote: {self.quote}"
